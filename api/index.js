7faecfdc7e1ed383fcf31571fbe3d08f2b0985e3
Cmakota <guilherme.m.ignacio@gmail.com>
2026-02-18 15:42:07 +0000

    tells vercel that requests should go to my proxy endpoint

---
+++ api/index.js
@@ -0,0 +1,15 @@
+const cors_proxy = require('cors-anywhere');
+
+// Create the proxy server instance
+const proxy = cors_proxy.createServer({
+    originWhitelist: [], // Allow all for now
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+});
+
+// This is the entry point for Vercel Serverless Functions
+module.exports = (req, res) => {
+    // This trick handles the request routing
+    req.url = req.url.replace(/^\/api/, ''); 
+    proxy.emit('request', req, res);
+};
