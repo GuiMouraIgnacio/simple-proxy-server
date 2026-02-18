528ec45169ce8dc05fa7c05d838035877cfcc5e9
Cmakota <guilherme.m.ignacio@gmail.com>
2026-02-18 16:16:38 +0000

    changes proxy approach to url query param

--- api/index.js
+++ api/index.js
@@ -1,27 +1,20 @@
-const cors_proxy = require('cors-anywhere');
-
-const proxy = cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins
-    requireHeader: [],   // Allow testing in browser
-    removeHeaders: ['cookie', 'cookie2']
-});
-
 module.exports = (req, res) => {
-    console.log('received req...')
-    // 1. Set the CORS headers so the browser (JSFiddle) is happy
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Origin, Accept, Authorization');
-    console.log('...added headers...')
+    const targetUrl = req.query.url;
 
-    // 2. Handle the "Preflight" (OPTIONS) request immediately
-    if (req.method === 'OPTIONS') {
-        res.status(200).end();
-        console.log('...was OPTIONS, finished now.')
+    if (!targetUrl) {
+        res.status(400).send("Missing url parameter");
         return;
     }
 
-    console.log('...was GET, sending to proxy.')
-    // 4. Pass the request to the proxy
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+
+    req.url = targetUrl; 
     proxy.emit('request', req, res);
 };
